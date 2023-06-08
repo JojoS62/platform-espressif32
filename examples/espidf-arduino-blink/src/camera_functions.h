9d141af2108cffb6e1bab015cc332e5b747485e3
Johannes Stratmann <sn2010@onlinehome.de>
2023-06-08 21:17:26 +0200

    inital commit with new testcode

---
+++ examples/espidf-arduino-blink/src/camera_functions.h
@@ -0,0 +1,141 @@
+#include <Arduino.h>
+camera_config_t config;
+
+void camera_config_gray(){
+    pinMode(CAMERA_LED_GPIO, OUTPUT);                                                               // LED
+    config.ledc_channel = LEDC_CHANNEL_0;
+    config.ledc_timer   = LEDC_TIMER_0;
+    config.pin_d0       = Y2_GPIO_NUM;
+    config.pin_d1       = Y3_GPIO_NUM;
+    config.pin_d2       = Y4_GPIO_NUM;
+    config.pin_d3       = Y5_GPIO_NUM;
+    config.pin_d4       = Y6_GPIO_NUM;
+    config.pin_d5       = Y7_GPIO_NUM;
+    config.pin_d6       = Y8_GPIO_NUM;
+    config.pin_d7       = Y9_GPIO_NUM;
+    config.pin_xclk     = XCLK_GPIO_NUM;
+    config.pin_pclk     = PCLK_GPIO_NUM;
+    config.pin_vsync    = VSYNC_GPIO_NUM;
+    config.pin_href     = HREF_GPIO_NUM;
+    config.pin_sccb_sda = SIOD_GPIO_NUM;
+    config.pin_sccb_scl = SIOC_GPIO_NUM;
+    config.pin_pwdn     = PWDN_GPIO_NUM;
+    config.pin_reset    = RESET_GPIO_NUM;
+    config.xclk_freq_hz = 20000000;
+    config.pixel_format = PIXFORMAT_GRAYSCALE;                                                    // Graustufen Bild
+    config.frame_size   = FRAMESIZE_CIF;                                                        // Auflösung 96x96 Pixel
+    config.jpeg_quality = 12;                                                                     // Qualität 
+    config.fb_count     = 1;                                                                      // Anzahl Framerbuffer
+    config.fb_location = CAMERA_FB_IN_PSRAM;
+    config.fb_location = CAMERA_FB_IN_DRAM;
+    // config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+       
+    esp_err_t err = esp_camera_init(&config);                                                      // camera init
+    if (err != ESP_OK) {
+        Serial.printf("Camera init failed with error 0x%x", err);
+        return;
+    }
+    sensor_t *s = esp_camera_sensor_get();
+    // initial sensors are flipped vertically and colors are a bit saturated
+    s->set_vflip(s, 1);        // flip it back
+    s->set_brightness(s, 1);   // up the blightness just a bit
+    s->set_saturation(s, -2);  // lower the saturation
+    //s->set_framesize(s, FRAMESIZE_QVGA); // drop down frame size for higher initial frame rate
+
+}
+
+void camera_config_jpg(){
+    pinMode(CAMERA_LED_GPIO, OUTPUT);                                                               // LED
+    config.ledc_channel = LEDC_CHANNEL_0;
+    config.ledc_timer   = LEDC_TIMER_0;
+    config.pin_d0       = Y2_GPIO_NUM;
+    config.pin_d1       = Y3_GPIO_NUM;
+    config.pin_d2       = Y4_GPIO_NUM;
+    config.pin_d3       = Y5_GPIO_NUM;
+    config.pin_d4       = Y6_GPIO_NUM;
+    config.pin_d5       = Y7_GPIO_NUM;
+    config.pin_d6       = Y8_GPIO_NUM;
+    config.pin_d7       = Y9_GPIO_NUM;
+    config.pin_xclk     = XCLK_GPIO_NUM;
+    config.pin_pclk     = PCLK_GPIO_NUM;
+    config.pin_vsync    = VSYNC_GPIO_NUM;
+    config.pin_href     = HREF_GPIO_NUM;
+    config.pin_sccb_sda = SIOD_GPIO_NUM;
+    config.pin_sccb_scl = SIOC_GPIO_NUM;
+    config.pin_pwdn     = PWDN_GPIO_NUM;
+    config.pin_reset    = RESET_GPIO_NUM;
+    config.xclk_freq_hz = 20000000;
+    config.pixel_format = PIXFORMAT_JPEG; 
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;                                                                     // Qualität 
+    config.fb_count     = 1;                                                                      // Anzahl Framerbuffer
+    config.fb_location = CAMERA_FB_IN_DRAM;
+    // config.fb_location = CAMERA_FB_IN_PSRAM;
+    config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+    esp_err_t err = esp_camera_init(&config);                                                      // camera init
+    if (err != ESP_OK) {
+        Serial.printf("Camera init failed with error 0x%x", err);
+        return;
+    }
+
+    sensor_t *s = esp_camera_sensor_get();
+    // initial sensors are flipped vertically and colors are a bit saturated
+    s->set_vflip(s, 1);        // flip it back
+    s->set_brightness(s, 1);   // up the blightness just a bit
+    s->set_saturation(s, -2);  // lower the saturation
+
+ 
+    //s->set_framesize(s, FRAMESIZE_QVGA); // drop down frame size for higher initial frame rate
+}
+
+
+camera_fb_t * fb_gray = NULL;                                                                     // Data structure of camera frame buffer
+camera_fb_t * fb_jpg = NULL;                                                                      // Data structure of camera frame buffer
+
+bool change_Picture_Format_to_JPEG() {
+  sensor_t *s = esp_camera_sensor_get();
+  if (s == NULL) {
+    Serial.println("Error: problem reading camera sensor settings");
+    return 0;
+  }
+  
+  s->set_pixformat(s,PIXFORMAT_JPEG); 
+  s->set_framesize(s,FRAMESIZE_SVGA);
+
+    esp_camera_change_resolution(&config, FRAMESIZE_SVGA);
+
+
+  return 1;
+}
+
+bool change_Picture_Format_to_GRAYSCALE() {
+  sensor_t *s = esp_camera_sensor_get();
+  if (s == NULL) {
+    Serial.println("Error: problem reading camera sensor settings");
+    return 0;
+  }
+  s->set_pixformat(s,PIXFORMAT_GRAYSCALE); 
+  s->set_framesize(s,FRAMESIZE_CIF);
+
+  esp_camera_change_resolution(&config, FRAMESIZE_CIF);
+
+  return 1;
+}
+
+void take_Camera_Image_JPG(){                                                                         // Bild aufnehmen
+  //change_Picture_Format_to_JPEG();
+  fb_jpg = esp_camera_fb_get();                                                                       // Bild in Framerbuffer
+  if (!fb_jpg){                                                                                       // Bei Fehler
+    Serial.println("photo failed");
+    while(1);                                                                                     //stop
+  }
+}
+
+void take_Camera_Image_GRAY(){                                                                         // Bild aufnehmen
+  //change_Picture_Format_to_GRAYSCALE();
+  fb_gray = esp_camera_fb_get();                                                                       // Bild in Framerbuffer
+  if (!fb_gray){                                                                                       // Bei Fehler
+    Serial.println("photo failed");
+    while(1);                                                                                     //stop
+  }
+}
---
+++ examples/espidf-arduino-blink/src/main.cpp
@@ -0,0 +1,77 @@
+#include <Arduino.h>
+#include <esp_camera.h>                                                                           // ESP Camera https://github.com/espressif/esp32-camera
+#include "camera_pins.h"                                                                          // Camera Pins OV3660
+#include "camera_config.h"                                                                        // Camera config
+#include "camera_functions.h"                                                                     // Camera Funktionen
+
+unsigned long Timer_Millis;
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  delay(3000);
+  Serial.println("setup start V1");
+  
+  camera_config_jpg();
+  // camera_config_gray();
+  
+  Serial.println("setup done");
+  //camera_config_jpg();
+}
+
+void Take_Pictures(){                                                           // Aktualisieren Winsocks Clients
+  if ((Timer_Millis + 2000) < millis()){                                                 // Alle ?? mS
+    Serial.print("Start ms ");
+    Serial.println(millis());
+
+    // esp_camera_deinit();
+    // camera_config_jpg();
+    
+    change_Picture_Format_to_JPEG();
+    
+    take_Camera_Image_JPG();                                                                       // Bild aufnehmen JPG
+    Serial.print("Jpg Buffer Len ");
+    Serial.println(fb_jpg->len);
+    esp_camera_fb_return(fb_jpg);
+    take_Camera_Image_JPG();                                                                       // Bild aufnehmen JPG
+    Serial.print("Jpg Buffer Len ");
+    Serial.println(fb_jpg->len);
+    esp_camera_fb_return(fb_jpg);
+    take_Camera_Image_JPG();                                                                       // Bild aufnehmen JPG
+    Serial.print("Jpg Buffer Len ");
+    Serial.println(fb_jpg->len);
+    esp_camera_fb_return(fb_jpg);
+    fb_jpg = NULL;
+
+    //esp_camera_deinit();
+    //camera_config_gray();
+
+#if 1
+    change_Picture_Format_to_GRAYSCALE();
+    
+    take_Camera_Image_GRAY();
+    Serial.print("Gray Buffer Len ");
+    Serial.println(fb_gray->len);
+    esp_camera_fb_return(fb_gray);
+    take_Camera_Image_GRAY();
+    Serial.print("Gray Buffer Len ");
+    Serial.println(fb_gray->len);
+    esp_camera_fb_return(fb_gray);
+    take_Camera_Image_GRAY();
+    Serial.print("Gray Buffer Len ");
+    Serial.println(fb_gray->len);
+    esp_camera_fb_return(fb_gray);
+    fb_gray = NULL;
+    Serial.print("Ende ms ");
+    Serial.println(millis());
+#endif
+
+    Timer_Millis = millis();                                                            // Zeit merken
+  }
+}
+
+void loop() {
+  Take_Pictures();
+}  
+
